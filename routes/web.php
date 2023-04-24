c3da114d8f6c2e9d46afc5a83d3ff0cbaf82d6c1
Ab_jimie <102118435+Bem20-james@users.noreply.github.com>
2023-04-24 11:59:24 +0100

    app setup.jimie

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::namespace('App\Http\Controllers\web')->group(function(){
+    Route::get('/', [HomeController::class, 'home'])->name('home');
+    Route::get('/services', [HomeController::class, 'services'])->name('services');
+    Route::get('/about', [HomeController::class, 'about'])->name('about');
+    Route::get('/blog', [HomeController::class, 'blog'])->name('blog');
+    Route::get('/categories', [HomeController::class, 'categories'])->name('categories');
+    Route::get('/testimonials', [HomeController::class, 'testimonials'])->name('testimonials');
+    Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+
+    // Auth Routes
+    Route::get('/login', [HomeController::class, 'login'])->name('login');
+    Route::get('/register', [HomeController::class, 'register'])->name('register');
+    Route::post('/register/process', [AuthController::class, 'registerUser'])->name('reg.process');
+    Route::post('/login/process', [AuthController::class, 'loginUser'])->name('login.process');
+    Route::post('/logout/user', [UserController::class, 'logout'])->name('logout');
+
+});
